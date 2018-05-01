973df5b34a7362d31a2ee641d3c733f60be62677
Chris <just_chris@web.de>
2018-05-01 23:22:09 +0200

    Add month display + update colors and positions

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var radius
 var typeface
+var months
 
 function preload(){
     typeface = loadFont ("https://justchrizz.github.io/ticktack/assets/SF-Pro-Display-Ultralight.otf")
@@ -12,6 +13,8 @@ function setup() {
     angleMode(DEGREES)
     //für rotation mit gradzahlen
     updateRadius()
+
+    months = ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez']
 }
 
 function draw() {
@@ -26,40 +29,49 @@ function draw() {
 
     ellipse(0, 0, radius * 2, radius * 2)
     ellipse(0, 0, radius * 1.8, radius * 1.8)
+    ellipse(0, 0, radius * 1.6, radius * 1.6)
     ellipse(0, 0, radius * 1.4, radius * 1.4)
-    ellipse(0, 0, radius * 1, radius * 1)
 
-    stroke(192, 100, 85)
-    
-    strokeWeight(radius/100) // "radius/100" damit linien auch responive
+   
 
 
     // sekundenzeiger
+    stroke(202, 100, 85)
+    strokeWeight(radius/100) // "radius/100" damit linien auch responive
+
     var date = new Date()
     var milliseconds = date.getMilliseconds()
 
     arc(0, 0, radius * 2, radius * 2, 0, map(second(), 0, 60, 0, 360) + map(milliseconds, 0, 1000, 0, 360 / 60))
 
     // minutenzeiger
+    stroke(192, 100, 85)
+    strokeWeight(radius/90)
     arc(0, 0, radius * 1.8, radius * 1.8, 0, map(minute(), 0, 60, 0, 360) + map(second(), 0, 60, 0, 360 / 60))
 
     // stundenzeiger
-    arc(0, 0, radius * 1.4, radius * 1.4, 0, map(hour(), 0, 24, 0, 360 * 2) + map(minute(), 0, 60, 0, 360 / 12))
+    stroke(182, 100, 85)
+    strokeWeight(radius/80)
+    arc(0, 0, radius * 1.6, radius * 1.6, 0, map(hour(), 0, 24, 0, 360 * 2) + map(minute(), 0, 60, 0, 360 / 12))
 
     // tagzeiger
-    arc(0, 0, radius * 1, radius * 1, 0, map(day(), 0, 30, 0, 360))
+    stroke(172, 100, 85)
+    strokeWeight(radius/70)
+    arc(0, 0, radius * 1.4, radius * 1.4, 0, map(day(), 0, 30, 0, 360))
+    
+
 
     push()
     rotate(90)
 
     textSize(radius / 10);
-    textAlign(CENTER, CENTER);
+    textAlign(CENTER, CENTER)
     textFont(typeface)
 
-    fill(0, 0, 100);
-    noStroke();
+    fill(0, 0, 100)
+    noStroke()
 
-    text(appendZero(day()) + '. ' + appendZero(month()) + '. | ' + appendZero(hour()) + ':' + appendZero(minute()) + ':' + appendZero(second()), 0, 0)
+    text(appendZero(day()) + '. ' + months[month() - 1] + ' | ' + appendZero(hour()) + ':' + appendZero(minute()) + ':' + appendZero(second()), 0, 0)
 
     pop()
 
